9c234d6679f49393e6b3a253dab179cec65a1c65
Niklas Frondorf <niklas.frondorf@web.de>
2022-10-08 16:23:48 +0200

    added temporary asm files to test

---
+++ intel-4004-sim/input3.asm
@@ -0,0 +1,38 @@
+START   FIM 4P, 0
+        SRC 4P
+        RDR
+        XCH 0
+        INC 8
+        SRC 4P
+        RDR
+        XCH 1
+        JMS AND
+        XCH 2
+        WMP
+        JUN PC
+        NOP
+PC=*
+*=104
+.BYTE 5
+AND     CLB
+        XCH 2
+        LDM 4
+AND_3   XCH 0
+        RAR
+        XCH 0
+        JCN CZ, ROTR1
+        XCH 1
+        RAR
+        XCH 1
+ROTR2   XCH 2
+        RAR
+        XCH 2
+        DAC
+        JCN ANZ, AND_3
+        BBL 0
+ROTR1   XCH 1
+        RAR
+        XCH 1
+        CLC
+        JUN ROTR2
+ANZ=12
