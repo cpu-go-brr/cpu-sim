ee6bf8ee3a4f62b77cc4b7d63aff6fae7f268a75
Niklas Frondorf <niklas.frondorf@web.de>
2022-10-12 15:11:56 +0200

    added functionality of labels in asm code

--- intel-4004-sim/src/parsing.cpp
+++ intel-4004-sim/src/parsing.cpp
@@ -5,6 +5,7 @@
 #include <regex>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 /* actual parsing of string
         cases to parse:
@@ -27,14 +28,43 @@ std::vector<std::string> op_codes = {
     "WR3", "SBM", "RDM", "RDR", "ADM", "RD0", "RD1", "RD2", "RD3", "CLB", "CLC",
     "IAC", "CMC", "CMA", "RAL", "RAR", "TCC", "DAC", "TCS", "STC", "DAA", "KBP", "DCL"};
 
-// map of labels defined in the asm code
-std::map<std::string, int> labels;
-
 // map of variables defined in the asm code
 std::map<std::string, int> vars;
 
-// // count addresses
-// int address_count = 0;
+std::vector<std::string> two_word_inst = {"SRC", "FIN", "JIN", "INC", "ADD", "SUB", "LD", "XCH", "BBL", "LDM", "JUN", "JMS"};
+std::vector<std::string> three_word_inst = {"JCN", "FIM", "ISZ"};
+
+std::map<std::string, int> one_word_inst_map = {
+    {"NOP", 0b00000000},
+    {"WRM", 0b11100000},
+    {"WMP", 0b11100001},
+    {"WRR", 0b11100010},
+    {"WR0", 0b11100100},
+    {"WR1", 0b11100101},
+    {"WR2", 0b11100110},
+    {"WR3", 0b11100111},
+    {"SBM", 0b11101000},
+    {"RDM", 0b11101001},
+    {"RDR", 0b11101010},
+    {"ADM", 0b11101011},
+    {"RD0", 0b11101100},
+    {"RD1", 0b11101101},
+    {"RD2", 0b11101110},
+    {"RD3", 0b11101111},
+    {"CLB", 0b11110000},
+    {"CLC", 0b11110001},
+    {"IAC", 0b11110010},
+    {"CMC", 0b11110011},
+    {"CMA", 0b11110100},
+    {"RAL", 0b11110101},
+    {"RAR", 0b11110110},
+    {"TCC", 0b11110111},
+    {"DAC", 0b11111000},
+    {"TCS", 0b11111001},
+    {"STC", 0b11111010},
+    {"DAA", 0b11111011},
+    {"KBP", 0b11111100},
+    {"DCL", 0b11111101}};
 
 const std::string WHITESPACE = " \n\r\t\f\v";
 
@@ -60,8 +90,8 @@ int string_to_integer(std::string str)
     int temp = 0;
     for (int i = 0; i < str.length(); i++)
     {
-        if (str[i] - '0'){
-
+        if (str[i] - '0')
+        {
         }
         temp = temp * 10 + (str[i] - '0');
     }
@@ -107,89 +137,96 @@ bool in_array(std::string str, const std::vector<std::string> array)
     return std::find(array.begin(), array.end(), str) != array.end();
 }
 
-std::vector<int> single_instruction_to_object_code(std::vector<std::string> instruction)
+int parse_value_from_argument(std::string arg)
 {
-    std::vector<std::string> two_word_inst = {"SRC", "FIN", "JIN", "INC", "ADD", "SUB", "LD", "XCH", "BBL", "LDM", "JUN", "JMS"};
-    std::vector<std::string> three_word_inst = {"JCN", "FIM", "ISZ"};
+    if(arg.find(",") != std::string::npos)
+    {
+        arg = arg.substr(0, arg.find(","));
+    }
 
-    std::map<std::string, int> one_word_inst_map = {
-        {"NOP", 0b00000000},
-        {"WRM", 0b11100000},
-        {"WMP", 0b11100001},
-        {"WRR", 0b11100010},
-        {"WR0", 0b11100100},
-        {"WR1", 0b11100101},
-        {"WR2", 0b11100110},
-        {"WR3", 0b11100111},
-        {"SBM", 0b11101000},
-        {"RDM", 0b11101001},
-        {"RDR", 0b11101010},
-        {"ADM", 0b11101011},
-        {"RD0", 0b11101100},
-        {"RD1", 0b11101101},
-        {"RD2", 0b11101110},
-        {"RD3", 0b11101111},
-        {"CLB", 0b11110000},
-        {"CLC", 0b11110001},
-        {"IAC", 0b11110010},
-        {"CMC", 0b11110011},
-        {"CMA", 0b11110100},
-        {"RAL", 0b11110101},
-        {"RAR", 0b11110110},
-        {"TCC", 0b11110111},
-        {"DAC", 0b11111000},
-        {"TCS", 0b11111001},
-        {"STC", 0b11111010},
-        {"DAA", 0b11111011},
-        {"KBP", 0b11111100},
-        {"DCL", 0b11111101}
-    };
-    std::string command = instruction[0];
-    int first_arg;
-    int second_arg;
+    int res;
+    int base = 10;
 
-    if (in_array(command, two_word_inst)){
-        std::string tmp_first = instruction[1];
-
-        try{
-            first_arg = std::stoi(tmp_first);
-        }catch(std::invalid_argument& exc){
-            if(vars.find(tmp_first) != vars.end()){
-                first_arg = vars[tmp_first];
+    if (arg[0] == '$')
+    {
+        arg = arg.substr(1, arg.length());
+        for (size_t i = 0; i < arg.length(); i++)
+        {
+            int current_acsii = int(arg[i]);
+            if (!(30 <= current_acsii <= 39) && !(41 <= current_acsii <= 46) && !(61 <= current_acsii <= 66))
+            {
+                arg = arg.substr(0, i);
+                break;
             }
         }
-    }else if (in_array(command, three_word_inst)){
-        std::string tmp_first;
-        
-        if (instruction[1].find(",") != std::string::npos){
-            tmp_first = instruction[1].replace(instruction[1].length() - 1, 1, "");
-        }else{
-            tmp_first = instruction[1];
-        }
-
-        try{
-            first_arg = std::stoi(tmp_first);
-        }catch(std::invalid_argument& exc){
-            if(vars.find(tmp_first) != vars.end()){
-                first_arg = vars[tmp_first];
+        base = 16;
+    }
+    else if (arg[0] == '%')
+    {
+        arg = arg.substr(1, arg.length());
+        for (size_t i = 0; i < arg.length(); i++)
+        {
+            int current_acsii = int(arg[i]);
+            if (!(30 <= current_acsii <= 31))
+            {
+                arg = arg.substr(0, i);
+                break;
             }
         }
-
-        try{
-            second_arg = std::stoi(instruction[2]);
-        }catch(std::invalid_argument& exc){
-            if(vars.find(instruction[2]) != vars.end()){
-                second_arg = vars[instruction[2]];
+        base = 2;
+    }
+    else if((30 <= arg[0] <= 39))
+    {
+        for (size_t i = 0; i < arg.length(); i++)
+        {
+            int current_acsii = int(arg[i]);
+            if (!(30 <= current_acsii <= 39))
+            {
+                arg = arg.substr(0, i);
+                break;
             }
         }
     }
 
-    // std::cout << "first_arg:" << first_arg << "\n";
-    // std::cout << "first_arg:" << second_arg << "\n";
+    try{
+        return std::stoi(arg, nullptr, base);
+    }
+    catch(std::invalid_argument& exc)
+    {
+        if (vars.find(arg) != vars.end())
+        {
+            return vars[arg];
+        }
+    }
+    
+}
+
+std::vector<int> single_instruction_to_object_code(std::vector<std::string> instruction)
+{
+    std::string command = instruction[0];
+    int first_arg;
+    int second_arg;
+
+    if (in_array(command, two_word_inst))
+    {
+        first_arg = parse_value_from_argument(instruction[1]);
+    }
+    else if (in_array(command, three_word_inst))
+    {
+        first_arg = parse_value_from_argument(instruction[1]);
+        second_arg = parse_value_from_argument(instruction[2]);
+    }
 
     if (command == "JCN")
-    {
-        return std::vector<int>{(0b0001 << 4) + first_arg % 16, second_arg % 256};
+    {   
+        // std::cout << "--------------------------------\n";
+        // std::cout << "inst1:" << instruction[1] << "\n";
+        // std::cout << "inst2:" << instruction[2] << "\n\n";
+        // std::cout << "first_arg:" << std::hex << first_arg << "\n";
+        // std::cout << "second_arg:" << std::hex << second_arg << "\n";
+        // std::cout << "--------------------------------\n\n\n";
+        
+        return std::vector<int>{(0b0001 << 4) + (first_arg & 0b1111), (second_arg & 0b11111111)};
     }
     else if (command == "FIM")
     {
@@ -247,7 +284,8 @@ std::vector<int> single_instruction_to_object_code(std::vector<std::string> inst
     {
         return std::vector<int>{(0b1101 << 4) + (first_arg % 16)};
     }
-    else if (one_word_inst_map.find(command) != one_word_inst_map.end()){
+    else if (one_word_inst_map.find(command) != one_word_inst_map.end())
+    {
         return std::vector<int>{one_word_inst_map[command]};
     }
 }
@@ -262,14 +300,19 @@ std::vector<std::string> get_clean_instructions(std::string file_path)
     // read the asm file line by line
     while (getline(asm_instruction_file, asm_instruction))
     {
-        if (asm_instruction.find(".END") != std::string::npos ||
-            asm_instruction.find("$") != std::string::npos)
+        // remove comments from the line
+        asm_instruction = asm_instruction.substr(0, asm_instruction.find(";"));
+
+        int pos_first_char = asm_instruction.find_first_not_of(WHITESPACE);
+        if (pos_first_char != std::string::npos && asm_instruction[pos_first_char] == '$')
         {
             return cleaned_instructions;
         }
 
-        // remove comments from the line
-        asm_instruction = asm_instruction.substr(0, asm_instruction.find(";"));
+        if (asm_instruction.find(".END") != std::string::npos)
+        {
+            return cleaned_instructions;
+        }
 
         // regex for variables defined in the asm file
         std::regex rgx_variabels("^([^=*]+)\s*=\s*([^=*]+)$");
@@ -287,7 +330,7 @@ std::vector<std::string> get_clean_instructions(std::string file_path)
             std::string variable_value = trim(var_def.substr(var_def.find("=") + 1, var_def.size()));
 
             // insert variable and value into the 'vars' map
-            vars.insert({variable_name, std::stoi(variable_value)});
+            vars.insert({variable_name, parse_value_from_argument(variable_value)});
         }
         else if (trim(asm_instruction) != "")
         {
@@ -299,14 +342,73 @@ std::vector<std::string> get_clean_instructions(std::string file_path)
     return cleaned_instructions;
 }
 
-std::vector<int> clean_instructions_to_object_code(std::vector<std::string> instructions)
+std::vector<std::string> parse_labels(std::vector<std::string> cleaned_instructions)
+{
+    int address_count = 0;
+
+    std::regex rgx_variabels("^([^=*]+)\s*=\s*\*$");
+    std::smatch matches;
+
+    std::vector<std::string> new_instructions = {};
+
+    for (size_t i = 0; i < cleaned_instructions.size(); i++)
+    {
+        std::string current_line = cleaned_instructions[i];
+        std::vector<std::string> split_instruction = split(current_line);
+        if (in_array(split_instruction[0], op_codes))
+        {
+            std::string tmp_inst = split_instruction[0];
+                if(tmp_inst == "JCN" || tmp_inst == "FIM" || tmp_inst == "JUN" || tmp_inst == "JMS" || tmp_inst == "ISZ")
+            {
+                address_count += 2;
+            }
+            else
+            {
+                address_count += 1;
+            }
+            new_instructions.push_back(current_line);
+        }else if(current_line.find(".BYTE") != std::string::npos)
+        {
+            address_count += 1;
+            new_instructions.push_back(current_line);
+        }else if (std::regex_search(current_line, matches, rgx_variabels))
+        {   std::string var_def = matches[0].str();
+
+            std::string variable_name = trim(var_def.substr(0, var_def.find("=")));
+            vars.insert({variable_name, address_count});
+            new_instructions.push_back(current_line);
+        }else{
+            vars.insert({split_instruction[0], address_count});
+            if(split_instruction.size() > 1)
+            {
+                std::string tmp_inst = split_instruction[1];
+                if(tmp_inst == "JCN" || tmp_inst == "FIM" || tmp_inst == "JUN" || tmp_inst == "JMS" || tmp_inst == "ISZ")
+                {
+                    address_count += 2;
+                }
+                else
+                {
+                    address_count += 1;
+                }
+                }
+
+            if(split_instruction.size() > 1)
+            {
+                new_instructions.push_back(current_line.substr(current_line.find(split_instruction[1]), current_line.length()));
+            }
+        }
+    }
+    return new_instructions;
+}
+
+std::vector<int> clean_instructions_to_object_code(std::vector<std::string> cleaned_instructions)
 {
     std::vector<int> res;
 
-    for (size_t i = 0; i < instructions.size(); i++)
+    for (size_t i = 0; i < cleaned_instructions.size(); i++)
     {
-        std::cout << instructions[i] << "\n";
-        std::vector<std::string> split_instruction = split(instructions[i]);
+        // std::cout << instructions[i] << "\n";
+        std::vector<std::string> split_instruction = split(cleaned_instructions[i]);
 
         if (in_array(split_instruction[0], op_codes))
         {
@@ -319,7 +421,7 @@ std::vector<int> clean_instructions_to_object_code(std::vector<std::string> inst
         }
         else
         {
-            // std::cout << split_instruction[0] << "\n";
+            std::cout << split_instruction[0] << "\n";
             /*
              options:
                 1. starts with '.' => pragma
@@ -334,13 +436,22 @@ std::vector<int> clean_instructions_to_object_code(std::vector<std::string> inst
 
 void Intel4004Parser::parse(std::string file_path)
 {
+    
     std::vector<std::string> cleaned_instructions = get_clean_instructions(file_path);
+    cleaned_instructions = parse_labels(cleaned_instructions);
+
+    // std::cout << "Vars (hex):\n";
+    // for (auto i : vars)
+    // {
+    //     std::cout << "|" << i.first << "|" << std::hex << i.second << "|" << std::endl;
+    // }
+    
 
     std::vector<int> res = clean_instructions_to_object_code(cleaned_instructions);
     std::cout << "\n\n";
     for (size_t i = 0; i < res.size(); i++)
     {
-        std::cout << std::hex << res[i] << " ";
+        std::cout << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << res[i] << " ";
         // if (i % 2 != 0){
         //     std::cout << std::hex << res[i] << " ";
         // }else{
