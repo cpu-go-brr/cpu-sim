b91496016e3c039473f05ed77ff447dae567ceab
Kurt <t.k.g.winkler@gmail.com>
2022-11-09 22:48:54 +0100

    remove stl libraries

--- hardware_emulation/src/main.cpp
+++ hardware_emulation/src/main.cpp
@@ -1,23 +1,20 @@
 #include <zephyr.h>
- #include <vector>
-uint32_t led = (1u << 27); 
+uint32_t led = (1u << 27);
 
 void main(void)
 {
 
-    std::vector<int> a{1,1,1};
-    for(const auto& i: a)
-            printk("%d\n", i);
+    REG_PIOB_OER = led;
 
-    REG_PIOB_OER = led; 
+    printk("Hello there, %s\n", CONFIG_BOARD);
 
-    printk("Hello World, %s\n", CONFIG_BOARD);
-
-    while(true)
+    while (true)
     {
-        REG_PIOB_SODR = led; // Set Output Data Register, turns LED on 
-        k_sleep({10000});         // wait for a second    
-        REG_PIOB_CODR = led; // Clear Output Data Register, turns LED off 
-        k_sleep({10000});         // wait for a second    
+        REG_PIOB_SODR = led; // Set Output Data Register, turns LED on
+        k_sleep({10000});    // wait for a second
+        printk("ON\n");
+        REG_PIOB_CODR = led; // Clear Output Data Register, turns LED off
+        k_sleep({10000});    // wait for a second
+        printk("OFF\n");
     }
 }
