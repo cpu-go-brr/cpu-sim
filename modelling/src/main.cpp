311ed5a589a621ac15a28326021a114208a3b551
Kurt <t.k.g.winkler@gmail.com>
2022-11-10 21:00:28 +0100

    cleaned comments

--- modelling/src/main.cpp
+++ modelling/src/main.cpp
@@ -2,57 +2,17 @@
 #include "cpu.hpp"
 #include "matheval.hpp"
 #include "bitset.hpp"
+#include "fbitset.hpp"
 
 #define PATH "test_files/intel4004.yaml"
 
-#include <bitset>
 
 int main(int argc, char **argv)
 {
 
-    bitset three_initialized{3,1};
-
-    std::cout << three_initialized.val() << "\n";
-
-
-    // bitset a{{0b1010}, 4};
-    // bitset b{{1}, 1};
-    
-        // std::cout << (a,b).bin() << "\n";
     std::cout << PATH << "\n";
     auto desc = Description::CPU(PATH);
     desc.generate();
-// 
-    // Intel4004 cpu;
 
-    // bitset a{{0xFF,0xFF,0xFF}, 20};
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     std::cout << (a >> i).show() << "\n";
-    // }
-
-    //     bitset b{{0b10111}, 5};
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     std::cout << (b >> i).show() << "\n";
-    // }
-    // for (int i = 0; i < 255; i++)
-    // {
-    //     auto g = get(&cpu.memory[0], ACC) + bitset({1},4);
-    //     set(&cpu.memory[0], ACC, g);
-    // }
-
-
-    // std::cout << get(&cpu.memory[0], ACC).show() << "\n";
-
-    // auto n = YAML::Load("3");
-    // Description::Memory m("hi", n);
-    // m.size = 14;
-    // m.offset = 0;
-
-    // for (const auto a : m.getBitmask())
-    // {
-    //     std::cout << std::bitset<8>(a) << '\n';
-    // }
     return 0;
 }
