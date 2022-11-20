38a656184e86d12a3d44a918a36bc9ee59ca9c2e
Kurt <t.k.g.winkler@gmail.com>
2022-11-20 14:38:44 +0100

    use command argument parser

--- cgen/src/main.cpp
+++ cgen/src/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include "cpu.hpp"
+#include "argument_parser.hpp"
+#include "generator.hpp"
 
-#define PATH "test_files/intel4004.yaml"
-
+// #define PATH "test_files/intel4004.yaml"
 
 int main(int argc, char **argv)
 {
-    std::cout << PATH << "\n";
-    auto desc = Description::CPU(PATH);
-    desc.generate();
+    ArgumentParser parser(argc, argv);
+    auto in = parser.getIn();
+    auto out = parser.getOut();
+    auto mode = parser.getMode();
+    
+    // for(auto [key,pointer]: generators) std::cout << key << "\n";
+    std::cout << "Building " <<in << " to " << out << "(" << mode << ")" << "\n";
+    auto cpu = Description::CPU(in);
+    generators[mode](cpu, out);
+    // desc.generate(out);
 
     return 0;
 }
