da9f4bbc94120d442294c6c7ce7c512b61167a0b
Kurt <t.k.g.winkler@gmail.com>
2022-11-26 14:18:35 +0100

    lzgit - 2022-11-26T13:18:35.273Z

--- cgen/include/generator.hpp
+++ cgen/include/generator.hpp
@@ -1,27 +1,32 @@
 #pragma once
 #include <string>
 #include <map>
-#include "cpu.hpp"
 #include <filesystem>
+#include "cpu.hpp"
 
+//function pointer to a source code generator
 typedef void (*Generator)(CPUDescription::CPU, std::filesystem::path);
+//a map of source code generators
 inline std::map<std::string, Generator> generators = {};
+
+//a function to generate common files used in all generators
 void generateStandard(CPUDescription::CPU cpu, std::filesystem::path path);
 
 
-#define ADD_GENERATOR(VAR) class VAR##Generator\
-    {                                                            \
-        friend class VAR##GeneratorInitializer;                  \
-        struct VAR##GeneratorInitializer{                        \
-            VAR##GeneratorInitializer(){                         \
-                generators[#VAR] = generate;                     \
-    }                                                            \
-    }                                                            \
-    ;                                                            \
-    static VAR##GeneratorInitializer init;                       \
-                                                                 \
-    static void generate(CPUDescription::CPU cpu, std::filesystem::path out); \
-    }                                                            \
-    ;                                                            \
+//Makro to automatically generate Generator class and add it into the generator Map
+#define ADD_GENERATOR(VAR)                                                        \
+    class VAR##Generator                                                          \
+    {                                                                             \
+        friend class VAR##GeneratorInitializer;                                   \
+        struct VAR##GeneratorInitializer                                          \
+        {                                                                         \
+            VAR##GeneratorInitializer()                                           \
+            {                                                                     \
+                generators[#VAR] = generate;                                      \
+            }                                                                     \
+        };                                                                        \
+        static VAR##GeneratorInitializer init;                                    \
+                                                                                  \
+        static void generate(CPUDescription::CPU cpu, std::filesystem::path out); \
+    };                                                                            \
     VAR##Generator::VAR##GeneratorInitializer VAR##Generator::init;
-
